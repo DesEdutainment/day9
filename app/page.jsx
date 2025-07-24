50edc3a2408bf2b253bd68a8fb6ca591472de00b
DesEdutainment <desdevelopment1@gmail.com>
2025-07-24 05:36:17 -0700

    added homepage

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import NavComponent from "@/components/nav";
+
+export default function HomePage() {
+  return (
+    <div>
+      <NavComponent></NavComponent>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main entry point of the application.</p>
+    </div>
+  );
+}   
